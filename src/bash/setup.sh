3cc8d145bb3dbd29be9556b2bb99afefe03682b7
lucasvittal2 <lucasvittal@gmail.com>
2025-06-23 08:54:45 -0300

    feat: implementing setup env script, still in progress

--- src/bash/setup.sh
+++
@@ -1,4 +0,0 @@
-# echo provisioning infrastructure on azure cloud
-src/bash/provisioning.sh --environment dev --location canadacentral --resource-group apache-airflow-env --node-count 2 --subscription "Azure subscription 1" --vm-size Standard_D2ps_v6
-
-#setup airflow with helm
