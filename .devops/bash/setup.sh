365feb4530fbeaf8b48c5119e9a1f9aea9b6ae8e
lucasvittal2 <lucasvittal@gmail.com>
2025-06-23 08:54:42 -0300

    feat: implementing setup env script, still in progress

---
+++ .devops/bash/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# echo provisioning infrastructure on azure cloud
+
+.devops/bash/provisioning.sh --environment dev --location canadacentral --resource-group apache-airflow-env --node-count 2 --subscription "Azure subscription 1" --vm-size Standard_B2ps_v2
+kubectl create namespace ${AKS_AIRFLOW_NAMESPACE} --dry-run=client --output yaml | kubectl apply -f -
+#deploy airflow on AKS
+.devops/bash/deploy.sh \
+  --namespace "airflow" \
+  --service-account "airflow" \
+  --values-file "helm/values.yaml"\
+  --container-registry "airflowregistrydev"\
+  --port-forward 8082 \
+  --ci-mode
