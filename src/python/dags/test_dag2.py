705ce8a3c89dee220dfa7c0a4959040a51d6cdc5
lucasvittal2 <lucasvittal@gmail.com>
2025-06-28 07:51:54 -0300

    test

--- src/python/dags/test_dag2.py
+++ src/python/dags/test_dag2.py
@@ -1,15 +1,32 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.dummy import DummyOperator
-from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
-dag = DAG('hello_world', description='Hello World DAG',
-          schedule_interval='0 12 * * *',
-          start_date=datetime(2022, 8, 24), catchup=False)
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
 
 def print_hello():
+    """Simple Python function that prints hello"""
     print("Hello world!")
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 
-hello_operator
+with DAG(
+    dag_id='hello_world',
+    description='Hello World DAG',
+    schedule_interval='0 12 * * *',
+    start_date=datetime(2022, 8, 24),
+    catchup=False,
+    tags=['test', 'tutorial'],
+    default_args=default_args
+) as dag:
+
+    hello_operator = PythonOperator(
+        task_id='hello_task',
+        python_callable=print_hello,
+        doc_md="""Prints 'Hello world!' to the logs"""
+    )
+
+    hello_operator
