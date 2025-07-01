43c08163f65d365f493fa4b9a96ef476bc26bfb0
lucasvittal2 <lucasvittal@gmail.com>
2025-07-01 20:13:59 -0300

    just update setup.sh to manual user

--- .github/bash/setup.sh
+++ .github/bash/setup.sh
@@ -2,13 +2,19 @@
 
 
 # echo provisioning infrastructure on azure cloud
-
+#deploy on azure
 .github/bash/provisioning.sh \
 --environment dev \
 --location canadacentral  \
 --deploy-env azure --node-count 2 \
 --subscription "Azure subscription 1" \
 --vm-size Standard_B2ps_v2
+#deploy on local
+.github/bash/provisioning.sh \
+--environment dev \
+--deploy-env azure \
+--subscription "Azure subscription 1"
+
 
 kubectl create namespace ${AKS_AIRFLOW_NAMESPACE} --dry-run=client --output yaml | kubectl apply -f -
 #deploy airflow on AKS
